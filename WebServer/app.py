d4de781a6d3dd5ad3d5c719133c1a3908cc97500
Santiago Yepes <ysantiago@unicauca.edu.co>
2021-12-12 01:45:00 -0500

    CHANGE: reading and saving GPS data with temporal database. Bug fixed

--- WebServer/app.py
+++ WebServer/app.py
@@ -19,18 +19,20 @@ socketio = SocketIO(app)
 scheduler = BackgroundScheduler(timezone="America/Bogota", job_defaults={'max_instances': 4})
 
 db = './database/vehicledatabase.db'
+db_temp = './database/locationdatabase.db'
+
 recording = False
 firstRecording = True
-lat = Value('d', 1.0)
-lng = Value('d', 1.0)
-count = Value('i', 0)
 id_vehicle = Array('u', list('defaultID'))
 period = Value('d', 20)
 time_elapsed = Value('d', 0)
-lock = Lock()
 
 connection_db = None
 cursor_db = None
+writer = sqlite3.connect(db_temp, isolation_level=None)
+writer.execute('pragma journal_mode=wal;')
+reader = sqlite3.connect(db_temp, isolation_level=None)
+reader.execute('pragma journal_mode=wal;')
 
 data_to_show = { "accX": 0, "accY": 0, "accZ": 0,
 	"velAngX": 0, "velAngY": 0, "velAngZ": 0,
@@ -38,11 +40,12 @@ data_to_show = { "accX": 0, "accY": 0, "accZ": 0,
 	"lat": 0, "lng": 0
 	}
 
+
 def create_connection():
 	"""Database connection
 	"""
 	try:
-		conn = sqlite3.connect(db)
+		conn = sqlite3.connect(db, isolation_level=None)
 	except Error as e:
 		print(e)
 	return conn
@@ -59,23 +62,23 @@ def create_database():
 	db_connection.commit()
 	cursor.close()
 	db_connection.close()
+	
+	with open('./database/configDB_temp.sql', 'r') as sql_file:
+		sql_script = sql_file.read()	
+	writer.executescript(sql_script)
 		
-def reading_gps_data (lat, lng, l):
+
+def reading_gps_data (writer):
 	while True:
 		latitude, longitude = gps_module.read_GPS_position()
-		lat.value = latitude
-		lng.value = longitude
-		#print('lat: {} - lng: {}'.format(lat.value, lng.value))
-		'''
-		latitude, longitude = gps_module.read_GPS_position_2()
-		if (latitude != -1):
-			lat.value = latitude
-			lng.value = longitude
-			print('lat: {} - lng: {}'.format(longitude, longitude))
-		time.sleep(0.1)	
-		'''
+		writer.execute('''UPDATE gpslocation
+			SET id = 0, latitude = ?, longitude = ?
+			WHERE id=0''', [latitude,longitude] )		
+		#print('lat: {} - lng: {}'.format(latitude, longitude))
 
-def saving_task (lat, lng, conn, cursor, count, l, id_vehicle, t):
+
+def saving_task (conn, cursor, reader, id_vehicle, t):
+	
 	"""Save all captured data
 	This function captured and save data from all modules
 	(IMU, GPS, OBD-II), the data is saved into database
@@ -83,7 +86,7 @@ def saving_task (lat, lng, conn, cursor, count, l, id_vehicle, t):
 	Keyword arguments:
 	temp_data -- List with this variables [latitude, longitude, conditional]
 	"""
-	#l.acquire()
+
 	# Load Kinematic data
 	kinematic_data = mpu6050.read_data()
 	accX, accY, accZ = kinematic_data[0]
@@ -91,40 +94,35 @@ def saving_task (lat, lng, conn, cursor, count, l, id_vehicle, t):
 	magX, magY, magZ = 0, 0, 0 #kinematic_data[2]
 	
 	# Load GPS data
-	# lat and lng contains the position 
+	latitude = 0
+	longitude = 0
+	for row in reader.execute('SELECT * FROM gpslocation WHERE id=0'):
+		latitude = row[1]
+		longitude = row[2]
+	print('lat: {} - lng: {}'.format(latitude, longitude))
 	
 	# Insert data into database	
 	data = [100, "".join(id_vehicle.value), time.time(), 
 			accX, accY, accZ, 
 			velAngX, velAngY, velAngZ,
 			magX, magY, magZ,
-			lat.value, lng.value
+			latitude, longitude
 			]
 	print("Saving data at: ", time.strftime('%H:%M:%S',time.gmtime(time.time())))
 	
 	query = '''INSERT INTO vehicledata
 			(
-				idTrip,
-				idVehicle,
-				timestamp,
-				accX,
-				accY,
-				accZ,
-				velAngX,
-				velAngY,
-				velAngZ,
-				magX,
-				magY,
-				magZ,
-				latitude,
-				longitude
+				idTrip, idVehicle, timestamp, 
+				accX, accY,	accZ,
+				velAngX, velAngY, velAngZ,
+				magX, magY,	magZ,
+				latitude, longitude
 			)
 			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
 	
 	cursor.execute(query, data)
 	
 	if ((time.time()-t.value) > 0.2 ):
-		print('lat: {} - lng: {}'.format(lat.value, lng.value))
 		data_to_show["accX"]= accX
 		data_to_show["accY"]= accY
 		data_to_show["accZ"]= accZ
@@ -134,13 +132,11 @@ def saving_task (lat, lng, conn, cursor, count, l, id_vehicle, t):
 		data_to_show["magX"]= 0
 		data_to_show["magY"]= 0
 		data_to_show["magZ"]= 0
-		data_to_show["lat"]= lat.value
-		data_to_show["lng"]= lng.value
+		data_to_show["lat"]= latitude
+		data_to_show["lng"]= longitude
 		send_data(data_to_show)
 		t.value = time.time()
 		
-	#l.release()
-
 
 def send_data(data_to_show):
 	socketio.emit('vehicleData', json.dumps(data_to_show))
@@ -158,7 +154,7 @@ def index():
 
 @app.route('/recordingTask', methods=['POST'])
 def handleRecordingTask():
-	global recording, lat, lng, connection_db, cursor_db, count, lock, id_vehicle, period, time_elapsed
+	global recording, connection_db, cursor_db, id_vehicle, period, time_elapsed
 	request_data = request.get_json()
 	recording = request_data['recording']
 	id_vehicle.value = list(request_data['idVehicle'])
@@ -167,14 +163,14 @@ def handleRecordingTask():
 	response = 'Rpi is now {}'.format('recording' if (recording) else 'stopped')
 
 	if(recording):
-		global firstRecording, scheduler
+		global firstRecording, scheduler, reader
 		
 		connection_db = create_connection()
 		cursor_db = connection_db.cursor()
 		
 		time_elapsed.value = time.time()
 		scheduler.add_job(saving_task, 
-			args=[lat,lng,connection_db,cursor_db,count,lock,id_vehicle,time_elapsed],
+			args=[connection_db, cursor_db, reader, id_vehicle, time_elapsed],
 			trigger='interval', seconds=period.value, id="saving_task")
 		
 		if (firstRecording):
@@ -202,9 +198,10 @@ def handle_connection(data):
 if __name__ == '__main__':
 	try:
 		create_database()
-		p = Process(target=reading_gps_data, args=(lat,lng,lock))
+		p = Process(target=reading_gps_data, args=(writer,), daemon=True)
 		p.start()
 		socketio.run(app, debug=True, port=8080, host='0.0.0.0')
-		# app.run(debug=True, port=80, host='0.0.0.0')
+		# app.run(debug=True, port=80, host='0.0.0.0
+		
 	except KeyboardInterrupt:
 		p.close()
